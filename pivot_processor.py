ce72212e9f196a2a4b58e827de5ff6a496cb9678
TTTriste06 <yunhan.yang08@gmail.com>
2025-07-21 10:04:08 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -116,8 +116,6 @@ class PivotProcessor:
         # 删除所有数值列（除前3列）都为 0 或空的行
         value_cols = main_df.columns[3:]  # 假设前三列为识别字段
         main_df = main_df[~(main_df[value_cols].fillna(0) == 0).all(axis=1)]
-        
-
 
         st.write(main_df)
 
